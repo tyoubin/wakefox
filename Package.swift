11281e69af23ec35ca761c2b9610df8800c2345d
tyoubin <2299309+tyoubin@users.noreply.github.com>
2026-09-22 21:19:49 +0800

    ci: target macOS 15 runners

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import PackageDescription
 let package = Package(
     name: "WakeFox",
     platforms: [
-        .macOS(.v13)
+        .macOS(.v15)
     ],
     products: [
         .executable(name: "WakeFox", targets: ["WakeFox"])
